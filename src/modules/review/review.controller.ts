2bd7c649b32b94b136a3855fb3d78c0b6517c902
tan.nguyen <tan.nguyen@furucrm.com>
2021-06-01 22:08:25 +0700

    update review and logout

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Session } from '@nestjs/common';
 import { ActionResponseService } from '../actionResponse/actionresponse.service';
+import { UserSession } from '../authen/dto/authen.dto';
 import { UserReview } from './dto/review.dto';
 import { Review } from './review.model';
 import { ReviewService } from './review.service';
@@ -11,6 +12,29 @@ export class ReviewController {
     private readonly actionResponseService: ActionResponseService
   ) {}
 
+  @Get()
+  async fetchReview(
+    @Session() { user }: { user: UserSession },
+    @Query()
+    { offset, limit, sort }: { offset: number; limit: number; sort: string }
+  ): Promise<ActionResponseService> {
+    // const { id }: { id: number } = user;
+    try {
+      const id = 1;
+      const data: Review[] = await this.reviewService.getRatingMovie(
+        id,
+        offset,
+        limit
+      );
+
+      return this.actionResponseService.responseApi(true, data, '');
+    } catch (error) {
+      console.log(error);
+    }
+
+    return this.actionResponseService.responseApi(true, [], 'error');
+  }
+
   @Get(':id')
   async fetchReviewsMovie(@Param('id') movieId: number) {
     try {
@@ -21,6 +45,28 @@ export class ReviewController {
         raw: true
       });
     } catch (error) {}
+  }
+
+  @Get('user')
+  async fetchReviewInformation(
+    @Session() { user }: { user: UserSession },
+    @Query() { limit }: { limit: number }
+  ) {
+    const { id }: { id: number } = user;
+    try {
+      const totalBookmark: number = await this.reviewService.countMovie(id);
+      const page = Math.ceil(totalBookmark / limit) | 1;
+
+      return this.actionResponseService.responseApi(
+        true,
+        { totalBookmark, page },
+        ''
+      );
+    } catch (error) {
+      console.log(error);
+    }
+
+    return this.actionResponseService.responseApi(true, '', 'error');
   }
 
   @Post(':id')
--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { Movie } from '../movie/movie.model';
 import { UserReview } from './dto/review.dto';
 import { Review } from './review.model';
 
@@ -7,6 +8,28 @@ export class ReviewService {
   constructor(
     @Inject('REVIEWS_REPOSITORY') private reviewRepository: typeof Review
   ) {}
+
+  async countMovie(id: number): Promise<number> {
+    return this.reviewRepository.count({ where: { id } });
+  }
+
+  async getRatingMovie(
+    id: number,
+    offset = 0,
+    limit = 10,
+    sort = 'name'
+  ): Promise<Review[]> {
+    const bookmarks: Review[] = await this.reviewRepository.findAll({
+      where: {
+        userId: id
+      },
+      include: [{ model: Movie, order: [sort] }],
+      offset: +offset,
+      limit: +limit
+    });
+
+    return bookmarks;
+  }
 
   async fetchReview(options = {}): Promise<Review[]> {
     return this.reviewRepository.findAll(options);
