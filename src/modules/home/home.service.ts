1711308fa71c13adb6ca6c2952f7c73b6ccb834c
tan.nguyen <tan.nguyen@furucrm.com>
2021-07-19 00:45:43 +0700

    update home page

--- src/modules/home/home.service.ts
+++ src/modules/home/home.service.ts
@@ -11,7 +11,7 @@ export class HomeService {
 
   async homePage() {
     const newMovie = await this.movieService.newestMovie(10);
-    const comingMovie = await this.movieService.comingMovie(10);
+    const comingMovie = await this.movieService.comingMovie(40);
     const topMovie = await this.movieService.topRatedMovie(10);
     const mostViewed = await this.movieService.mostViewed(10);
 
